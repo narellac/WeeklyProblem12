e8191602389706174f3baf0c2ff8f41731413958
Narella Calvente <narellacalvente@gmail.com>
2021-04-23 17:43:06 -0300

    New files

---
+++ operations.test.js
@@ -0,0 +1,84 @@
+import { TestResult } from "@jest/types";
+import * as operations from "./operations";
+
+//1
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+
+//2
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//3
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//4
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//5
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//6
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//7
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//8
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
+
+//9
+describe('function add positive numbers', () => {
+
+  test('sumar 5 + 4 es igual a 9', () => {
+  expect(operations.suma(5, 4)).toBe(9);
+  });
+
+});
